4d3703ad95a0e8ffda6dad1c401c205a2affde32
vickeykumar <kumarvickey45@gmail.com>
2018-03-05 01:14:04 +0530

    Cleaning..

--- src/js/node_modules/xterm/lib/utils/Generic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function contains(arr, el) {
-    return arr.indexOf(el) >= 0;
-}
-exports.contains = contains;
-;
-
-//# sourceMappingURL=Generic.js.map
