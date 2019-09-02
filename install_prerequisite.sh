b6376d19f131853830b19e1717f6212b13cdd09c
vickeykumar <kumarvickey45@gmail.com>
2019-09-02 20:07:36 +0530

    prerequisites script

---
+++ install_prerequisite.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#please run as root/sudo
+
+cd ~
+
+#install golang
+apt install golang
+
+#install npm
+apt install npm
+
+#install cling
+
+#install gointerpreter
+git clone https://github.com/vickeykumar/Go-interpreter.git
+cd Go-interpreter
+make install
+cd ..
+
+#install ipython2.7
+apt install ipython
+
+#install ipython3
+apt install ipython3
+
+#install Ruby(irb)
+apt install ruby
