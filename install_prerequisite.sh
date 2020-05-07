be530939281c152803e1f45add41bb00ae1c8251
vickeykumar <kumarvickey45@gmail.com>
2020-05-07 10:17:03 +0530

    prerequisite file update

--- install_prerequisite.sh
+++ install_prerequisite.sh
@@ -3,6 +3,10 @@
 
 cd ~
 
+# set cap to nsenter,gcc,g++
+# sudo setcap "cap_sys_admin,cap_sys_ptrace+ep" /usr/bin/nsenter && ./containers-from-scratch/main run 2 nsenter -n -t$$ /bin/bash
+# sudo setcap "cap_sys_admin,cap_sys_ptrace+ep" /usr/bin/arm-linux-gnueabihf-gcc-8
+
 #install golang
 apt install golang
 
@@ -11,6 +15,8 @@ apt install npm
 npm install npm@latest -g
 
 #install cling
+#use following to compile minimal cling
+#/usr/bin/ld.gold --strip-all --no-map-whole-files --no-keep-memory --no-keep-files-mapped $@ 
 # cling takes some time to init first instance, add below lines to rc.local(startup)
 #/usr/bin/cling 21321 .q > /dev/null 2>&1 &
 
