7f5c951a7f6e927ca8b77a8e8ea46a6cbaae19b8
vickey kumar <kumarvickey45@gmail.com>
2023-01-21 18:15:24 +0530

    Update install_prerequisite.sh

--- install_prerequisite.sh
+++ install_prerequisite.sh
@@ -6,6 +6,7 @@ cd ~
 
 # set cap to nsenter,gcc,g++
 setcap "cap_sys_admin,cap_sys_ptrace+ep" /usr/bin/nsenter 
+chown root.root /usr/bin/nsenter
 # && ./containers-from-scratch/main run 2 nsenter -n -t$$ /bin/bash
 # sudo setcap "cap_sys_admin,cap_sys_ptrace+ep" /usr/bin/arm-linux-gnueabihf-gcc-8
 # add export NODE_OPTIONS=--max_old_space_size=2048 to .bashrc
