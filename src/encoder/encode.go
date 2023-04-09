5fb07c3d9301db3cbd47213c93e15e63a91dda26
vickeykumar <kumarvickey45@gmail.com>
2023-04-09 05:04:10 +0000

    pid2jid encoder changed to use large prime number

--- src/encoder/encode.go
+++ src/encoder/encode.go
@@ -3,25 +3,49 @@ package encoder
 import (
 	"encoding/base64"
 	"log"
-	"strconv"
+	"crypto/rand"
+    	"math/big"
 )
+
+// use 32 bits for type int
+const SECRET_BITSIZE = 16
+
+func GenerateLargePrime() *big.Int {
+     // generate large prime
+
+    // Generate a random prime number
+    prime, err := rand.Prime(rand.Reader, SECRET_BITSIZE)
+    if err != nil {
+        log.Println("Error:", err)
+        // any prime number, probably we won't reach here
+        return  big.NewInt(65537)
+    }
+    
+    return prime
+}
 
 var RawStdEncoding = base64.StdEncoding.WithPadding(base64.NoPadding)
 
+var largesecret *big.Int = GenerateLargePrime()
+
+
 func EncodePID(pid interface{}) string {
-	ppid, _ := pid.(int)
-	return RawStdEncoding.EncodeToString([]byte(strconv.Itoa(ppid)))
+        ppid, _ := pid.(int)
+        message := big.NewInt(int64(ppid))
+        message = message.Mul(message, largesecret)
+        // send the encoded Bytes
+        return RawStdEncoding.EncodeToString(message.Bytes())
 }
 
 func DecodeToPID(jid string) int {
-	var pid int = -1
-	pidstr,err := RawStdEncoding.DecodeString(jid)
-	if err == nil {
-		pid, err = strconv.Atoi(string(pidstr))
-		if err == nil {
-			return pid
-		}
-	}
-	log.Println("invalid pid recieved :"+string(pidstr), err.Error())
-	return -1
+        pidbytes,err := RawStdEncoding.DecodeString(jid)
+        if err == nil {
+                decoded := new(big.Int).SetBytes(pidbytes)
+                decoded.Div(decoded, largesecret)
+                return int(decoded.Int64())
+        }
+        log.Println("invalid pid recieved :"+jid, string(pidbytes), err.Error())
+        return -1
 }
+
+
